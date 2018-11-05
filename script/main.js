2263f4955fd652acaf3bfa9dcbbfb18bac1c9756
Josip Maros <j.maros95@gmail.com>
2018-11-05 21:50:22 +0100

    grid created

--- script/main.js
+++ script/main.js
@@ -1,17 +1,38 @@
-document.addEventListener("load", function(){
 
 const gridSize = 16; 
+const squareHeightWidth = 100 / gridSize;
 
-var element = document.getElementById("container");
 
-square.className = "square";
-element.appendChild(square);
+var element = document.querySelector("#container")
+element.style.backgroundColor = "blue";
 
-for(i=0; i<gridSize; i++){
-    var square = document.createElement("div");
-    element.appendChild(square);
+var square = [];
+
+
+createGrid();
+
+
+
+function createGrid(){
+
+    for(i=0; i<gridSize; i++){
+        for(j=0; j<gridSize; j++){
+            square[j + gridSize * i] = document.createElement("div");
+            square[j + gridSize * i].style.backgroundColor = "white";
+            square[j + gridSize * i].style.height = squareHeightWidth + "%";
+            square[j + gridSize * i].style.width = squareHeightWidth + "%";
+            square[j + gridSize * i].setAttribute("id", "square" + "X" + i + "Y" +j);
+            square[j + gridSize * i].style.display = "inline-block";
+            element.appendChild(square[j + gridSize * i]);
+
+            square[j + gridSize * i].addEventListener("mouseenter", function(){
+                square.style.backgroundColor = "red";
+            });
+        }
+
+        
+    }
 }
-document.removeChild(element);
 
-});
+
 
