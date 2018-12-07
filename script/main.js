266d6facc9d5f24a8b66a0ea4157ec99dd11441f
Josip Maros <j.maros95@gmail.com>
2018-12-07 22:56:02 +0100

    Every pass through a div increments its background-color by 10%(Starting color white).

--- script/main.js
+++ script/main.js
@@ -3,7 +3,7 @@ let gridSize = 16;
 let squareHeightWidth = 100 / gridSize;
 
 
-var element = document.querySelector("#container")
+var element = document.querySelector("#content")
 element.style.backgroundColor = "white";
 
 let square = [];
@@ -14,6 +14,7 @@ createGrid();
 
 
 function createGrid(){
+    let mouseenterCounter = 0;
 
     for( let i=0; i<gridSize; i++){
         for( let j=0; j<gridSize; j++){
@@ -24,11 +25,14 @@ function createGrid(){
             square[j + gridSize * i].setAttribute("id", "square" + "X" + i + "Y" +j);
             square[j + gridSize * i].style.display = "inline-block";
             square[j + gridSize * i].style.verticalAlign = "top";
+            square[j + gridSize * i].mouseenterCounter = 0;
             element.appendChild(square[j + gridSize * i]);
 
             square[j + gridSize * i].addEventListener("mouseenter", function(){
+                square[j + gridSize * i].mouseenterCounter ++;
                 console.log("Entered:" + (j + gridSize * i));
-                square[j + gridSize * i].style.backgroundColor = "blue";
+                square[j + gridSize * i].style.opacity = square[j + gridSize * i].mouseenterCounter * 0.1;
+                square[j + gridSize * i].style.backgroundColor = "black";
             });
         }
 
